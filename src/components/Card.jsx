f10f4d688023e731fa12ad379f8d9e287e72ad3f
Samcode001 <93057570+Samcode001@users.noreply.github.com>
2023-04-24 12:35:36 +0530

    Some comments added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,32 +2,32 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useCart, useDispatchCart } from './ContextReducer';
 
 const Card = (props) => {
-  let options=props.options;
-  let priceOptions = Object.keys(options); //inbuilt function of javascript dealing with objects
+  let options = props.options;
+  let priceOptions = Object.keys(options); //inbuilt function of javascript which retrieve all the elements of  objects when use with map function
 
   let dispatch = useDispatchCart();
-  let data=useCart();
-  let priceRef=useRef(); // the price doenst has its default value so we have to refer it to the size option to take dafault values from there 
+  let data = useCart();
+  let priceRef = useRef(); // the price doenst has its default value so we have to refer it to the size option to take dafault values from there 
 
-  const [qty, setQty] = useState(1)
-  const [size, setSize] = useState("")
+  const [qty, setQty] = useState(1);
+  const [size, setSize] = useState("");
 
   const handleAddToCart = async () => {
 
     // The below codes is working for the update its check all the parameters 
-    let food=[]
-    for(const item of data){
-      if(item.id===props.foodItem._id){
-        food=item;
+    let food = []
+    for (const item of data) {
+      if (item.id === props.foodItem._id) {
+        food = item;
         break;
       }
     }
-    if(food!==[]){
-      if(food.size===size){
-        await dispatch({type:'UPDATE',id:props.foodItem._id,price:finalPrice,qty:qty})
+    if (food !== []) {
+      if (food.size === size) {
+        await dispatch({ type: 'UPDATE', id: props.foodItem._id, price: finalPrice, qty: qty })
         return
       }
-      else if(food.size !==size){
+      else if (food.size !== size) {
         await dispatch({
           type: "ADD",
           id: props.foodItem._id,
@@ -51,10 +51,10 @@ const Card = (props) => {
     })
   }
 
-let finalPrice=qty*parseInt(options[size]); 
-useEffect(()=>{
-  setSize(priceRef.current.value) // Setting the finalprice in each render
-},[]) 
+  let finalPrice = qty * parseInt(options[size]);
+  useEffect(() => {
+    setSize(priceRef.current.value) // Setting the finalprice in each render
+  }, [])
 
   return (
     <>
@@ -64,7 +64,7 @@ useEffect(()=>{
           <div className="card-body">
             <h5 className="card-title">{props.foodItem.name}</h5>
             <div className="container w-100">
-              <select className='m-2 h-100 bg-success rounded' onChange={(e)=>setQty(e.target.value)}>
+              <select className='m-2 h-100 bg-success rounded' onChange={(e) => setQty(e.target.value)}> {/* in here we are intializing the state of qty by getting the value from user selects from options */}
                 {Array.from(Array(6), (e, i) => { // Makes an array of 6 and retriveing the values
                   return (
                     <option key={i + 1} value={i + 1}>{i + 1}</option>
@@ -72,7 +72,7 @@ useEffect(()=>{
                 })}
               </select>
               {/* initializing the value of options or select in the state by onChange Method for myCart  */}
-              <select className='m-2 h-100 bg-success rounded ' ref={priceRef}  onChange={(e)=>setSize(e.target.value)}>
+              <select className='m-2 h-100 bg-success rounded ' ref={priceRef} onChange={(e) => setSize(e.target.value)}>
                 {priceOptions.map((data) => {
                   return <option key={data} value={data}>{data}</option>
                 })}
--- src/components/ContextReducer.jsx
+++ src/components/ContextReducer.jsx
@@ -6,17 +6,16 @@ const CartDispatchContext=createContext();
 const reducer=(state,action)=>{
 switch (action.type) {
     case "ADD":
-        return[...state,{
+        return[...state,{ // in here we are using the spread operator(copying & merging).without hampering hte state i.e the data we are adding new data in database in form of object.
             id:action.id,
             name:action.name,
             qty:action.qty,
             size:action.size,
-            price:action.price,
-            img:action.img
+            price:action.price
         }]
         case 'REMOVE':
             let newArr=[...state]  // In here we can't chnage the state beacause it will hamper the other case so in here we take a temporary var then return with that var
-            newArr.splice(action.index,1)
+            newArr.splice(action.index,1) // we are popping the element which present at index . The state is not changed but we are just giving an updated value os state data
             return newArr;
 
         // The Add and Remove functionality is same as it is in here in all the related apps but the Update functionality it's very specific that what app we are working on   
@@ -43,7 +42,7 @@ switch (action.type) {
 }
 
 export const CartProvider=({children})=>{
-    const [state, dispatch] = useReducer(reducer, [])
+    const [state, dispatch] = useReducer(reducer, []) // Initial state is empty array
     return(
         <CartDispatchContext.Provider value={dispatch}>
             <CartStateContext.Provider value={state}>
